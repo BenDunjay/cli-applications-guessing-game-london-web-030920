e7a9cac61c2a2fbb3dd3738b50f3a1ac5b39b532
Ben Dunjay <bendunjay@outlook.com>
2020-03-05 17:27:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,13 +1,13 @@
 require "pry"
 
 
-def run_guessing_game
+def run_guessing_game(array)
  computer_number = rand(6)+1
  persons_number = gets.split
  
   if computer_number == persons_number
     puts "You guessed the correct number!"
-  elsif computer_number > persons_number || computer_number < persons_number
+  elsif computer_number != persons_number
     puts "Sorry! The computer guessed #{computer_number}."
   else persons_number == "exit"
     puts "Goodbye!"
